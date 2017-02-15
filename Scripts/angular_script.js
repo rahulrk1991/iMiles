1fb60b7e95c29343af3b054b2f0c8c7504cc0316
rkhanvilkar <rahul.khanvilkar@oracle.com>
2017-02-16 02:17:22 +0530

    Added hiring test 1 jumbotron in online mock test page and resume upload button in account settings page

--- Scripts/angular_script.js
+++ Scripts/angular_script.js
@@ -373,6 +373,41 @@ var app = angular
             $scope.totalNumberOfTests = 0;
             $scope.accuracy = 0;
 
+            // Hiring Test : Set the date we're counting down to
+            var countDownDate = new Date("Feb 26, 2017 12:00:00").getTime();
+
+            // Update the count down every 1 second
+            var countdown_timer_function = setInterval(function() {
+
+              // Get todays date and time
+              var now = new Date().getTime();
+
+              // Find the distance between now an the count down date
+              var distance = countDownDate - now;
+
+              // Time calculations for days, hours, minutes and seconds
+              var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+              var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+              var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+              var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+              // Display the result in the element with id="demo"
+              document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+              + minutes + "m " + seconds + "s ";
+
+              // If the count down is finished, write some text 
+              if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("demo").innerHTML = "EXPIRED";
+              }
+            }, 1000);
+
+            $scope.$on("$locationChangeStart", function (event, next, current) {
+                
+                clearInterval(countdown_timer_function);
+
+            });
+
             var getAllMockTests = function() {
 
                 $http.get(mock_myresults_API)
