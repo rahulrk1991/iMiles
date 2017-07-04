3b4e6c10c54973397c9f217841b3fa8c891ad961
rkhanvilkar <rahul.khanvilkar@oracle.com>
2017-07-05 00:05:00 +0530

    HiringTest : Changed dates for new hiring test

--- Scripts/angular_script.js
+++ Scripts/angular_script.js
@@ -213,9 +213,9 @@ var app = angular
             $rootScope.rootScope_experience = -1;
 
             //Timer that appears in navigation bar
-            $rootScope.dateOfHiringTest = "Jun 25, 2017 14:00:00";
-            $rootScope.dateTillItCanBeGiven = "Jun 25, 2017 21:00:00"
-            $rootScope.dateOfNextHiringTest = "Jul 2, 2017 14:00:00";
+            $rootScope.dateOfHiringTest = "Jul 9, 2017 14:00:00";
+            $rootScope.dateTillItCanBeGiven = "Jul 9, 2017 21:00:00"
+            $rootScope.dateOfNextHiringTest = "Jul 16, 2017 14:00:00";
 
             $scope.dateOfHiringTest = $rootScope.dateOfHiringTest;
             $scope.dateTillItCanBeGiven = $rootScope.dateTillItCanBeGiven;
