d6d6efe95a52581455c30ead2694ae75da7dd1fe
root <root@ip-172-31-6-233.ap-south-1.compute.internal>
2017-08-08 19:25:18 +0000

    ui changes

--- Scripts/angular_script.js
+++ Scripts/angular_script.js
@@ -164,6 +164,9 @@ var app = angular
                 templateUrl: absolute_path+"OnlineMockTests/choose_a_test.html",
                 controller:"onlineMockTestsChooseTestController"
             })
+            
+	    $locationProvider.html5Mode(true);
+
          })
         .controller("OtherArtifactsController",function($scope){
 
